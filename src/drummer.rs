b4d238b615604dac64388d456a766513963d699a
Umberto Loria <umberto.loria@gmail.com>
2024-12-25 19:40:47 +0100

    Drummer is in town :)

---
+++ src/drummer.rs
@@ -0,0 +1,35 @@
+use midir::MidiOutputConnection;
+use std::thread::sleep;
+use std::time::Duration;
+
+// Durations
+pub const DUR_1_4: Duration = Duration::from_millis(1000);
+pub const DUR_1_8: Duration = Duration::from_millis(500);
+pub const DUR_1_16: Duration = Duration::from_millis(250);
+pub const DUR_1_32: Duration = Duration::from_millis(125);
+
+// Drum parts
+pub const DRUM_KICK: u8 = 1;
+pub const DRUM_HH: u8 = 2;
+pub const DRUM_SNARE: u8 = 4;
+
+// BPM config
+const BPM_DEFAULT: f64 = 60.0;
+
+pub struct Drummer<'a> {
+    pub conn: &'a mut MidiOutputConnection,
+    pub bpm: f64,
+}
+impl Drummer<'_> {
+    pub fn hit(&mut self, note: u8, instr: u8, duration: Duration) {
+        const NOTE_ON_MSG: u8 = 0x90;
+        const NOTE_OFF_MSG: u8 = 0x80;
+        const PROGRAM_CHANGE: u8 = 0xC0;
+        const VELOCITY: u8 = 0x70;
+        // We're ignoring errors in here
+        let _ = self.conn.send(&[PROGRAM_CHANGE, instr]);
+        let _ = self.conn.send(&[NOTE_ON_MSG, note, VELOCITY]);
+        sleep(duration.mul_f64(BPM_DEFAULT).div_f64(self.bpm));
+        let _ = self.conn.send(&[NOTE_OFF_MSG, note, VELOCITY]);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,35 @@
-use std::io::Write;
-use std::thread::sleep;
-use std::time::Duration;
-
+use crate::drummer::{Drummer, DRUM_HH, DRUM_KICK, DRUM_SNARE, DUR_1_8};
 use crate::midi_controller::init_midi_controller;
+use std::io::Write;
 
+mod drummer;
 mod midi_controller;
 
 fn main() {
+    // MIDI CONNECTION: START
     let midi_controller = init_midi_controller(Some(1)).expect("Unable to create midi controller");
     let mut conn = midi_controller.connect_and_get();
-    {
-        // Durations
-        const DUR_1_4: Duration = Duration::from_millis(1000);
-        const DUR_1_8: Duration = Duration::from_millis(500);
-        const DUR_1_16: Duration = Duration::from_millis(250);
-        const DUR_1_32: Duration = Duration::from_millis(125);
 
-        // Drum parts
-        const DRUM_KICK: u8 = 1;
-        const DRUM_HH: u8 = 2;
-        const DRUM_SNARE: u8 = 4;
+    // SONG INIT
+    let bpm: f64 = 120.0;
+    let mut drummer = Drummer {
+        conn: &mut conn,
+        bpm,
+    };
 
-        // BPM config
-        const BPM_DEFAULT: f64 = 60.0;
-        let bpm: f64 = 120.0;
-
-        // Define a new scope in which the closure `play_note` borrows conn_out, so it can be called easily
-        let mut play_note = |note: u8, instr: u8, duration: Duration| {
-            const NOTE_ON_MSG: u8 = 0x90;
-            const NOTE_OFF_MSG: u8 = 0x80;
-            const PROGRAM_CHANGE: u8 = 0xC0;
-            const VELOCITY: u8 = 0x70;
-            // We're ignoring errors in here
-            let _ = conn.send(&[PROGRAM_CHANGE, instr]);
-            let _ = conn.send(&[NOTE_ON_MSG, note, VELOCITY]);
-            sleep(duration.mul_f64(BPM_DEFAULT).div_f64(bpm));
-            let _ = conn.send(&[NOTE_OFF_MSG, note, VELOCITY]);
-        };
-
-        // First bar
-        for _ in 0..(2 * 4) {
-            play_note(7, DRUM_HH, DUR_1_8);
-        }
-        // Second bar
-        play_note(7, DRUM_KICK, DUR_1_8);
-        play_note(7, DRUM_HH, DUR_1_8);
-        play_note(7, DRUM_SNARE, DUR_1_8);
-        play_note(7, DRUM_HH, DUR_1_8);
-        play_note(7, DRUM_KICK, DUR_1_8);
-        play_note(7, DRUM_KICK, DUR_1_8);
-        play_note(7, DRUM_SNARE, DUR_1_8);
-        play_note(7, DRUM_HH, DUR_1_8);
+    // SONG PLAY
+    for _ in 0..(2 * 4) {
+        drummer.hit(7, DRUM_HH, DUR_1_8);
     }
-    println!("\nClosing connection");
-    // This is optional, the connection would automatically be closed as soon as it goes out of scope
+    drummer.hit(7, DRUM_KICK, DUR_1_8);
+    drummer.hit(7, DRUM_HH, DUR_1_8);
+    drummer.hit(7, DRUM_SNARE, DUR_1_8);
+    drummer.hit(7, DRUM_HH, DUR_1_8);
+    drummer.hit(7, DRUM_KICK, DUR_1_8);
+    drummer.hit(7, DRUM_KICK, DUR_1_8);
+    drummer.hit(7, DRUM_SNARE, DUR_1_8);
+    drummer.hit(7, DRUM_HH, DUR_1_8);
+
+    // MIDI CONNECTION: SHUT-DOWN
     conn.close();
-    println!("Connection closed");
 }
