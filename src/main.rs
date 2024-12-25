c615f156c417762447618d84f017283aa7d602f0
Umberto Loria <umberto.loria@gmail.com>
2024-12-25 21:39:18 +0100

    Sound Panel added

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-use crate::drummer::{Drummer, ParamSoundType};
 use crate::input::get_console_int_input;
 use crate::midi_controller::init_midi_controller;
+use crate::sound_panel::{ParamSoundType, SoundPanel};
 use std::io::Write;
 
 mod drummer;
 mod input;
 mod midi_controller;
+mod sound_panel;
 
 fn main() {
     // MIDI CONNECTION: START
@@ -13,29 +14,10 @@ fn main() {
     let mut conn = midi_controller.connect_and_get();
 
     // SONG INIT
-    let bpm: f64 = 120.0;
-    let mut drummer = Drummer {
-        conn: &mut conn,
-        bpm,
-    };
-
-    // SONG PLAY
-    /*
-    for _ in 0..(2 * 4) {
-        drummer.hit(7, DRUM_HH, DUR_1_8);
-    }
-    drummer.hit(7, DRUM_KICK, DUR_1_8);
-    drummer.hit(7, DRUM_HH, DUR_1_8);
-    drummer.hit(7, DRUM_SNARE, DUR_1_8);
-    drummer.hit(7, DRUM_HH, DUR_1_8);
-    drummer.hit(7, DRUM_KICK, DUR_1_8);
-    drummer.hit(7, DRUM_KICK, DUR_1_8);
-    drummer.hit(7, DRUM_SNARE, DUR_1_8);
-    drummer.hit(7, DRUM_HH, DUR_1_8);
-    */
+    let mut sound_panel = SoundPanel { conn: &mut conn };
 
     // SOUND SETTING
-    drummer.set_param_level(0, ParamSoundType::Level, 127);
+    sound_panel.set_param_level(0, ParamSoundType::Level, 127);
     loop {
         let value = get_console_int_input("Give me pitch value: ", 0, 127);
         if value == 0 {
@@ -43,7 +25,7 @@ fn main() {
         }
 
         // Before, it was: "drummer.send_cc_message(0, 26, sine_val_u8);"
-        drummer.set_param_level(0, ParamSoundType::Pitch, value);
+        sound_panel.set_param_level(0, ParamSoundType::Pitch, value);
     }
 
     // MIDI CONNECTION: SHUT-DOWN
---
+++ src/sound_panel.rs
@@ -0,0 +1,113 @@
+use midir::MidiOutputConnection;
+
+pub struct SoundPanel<'a> {
+    pub conn: &'a mut MidiOutputConnection,
+}
+impl SoundPanel<'_> {
+    pub fn set_sound_source_type(&mut self, channel: u8, sound_source_type: SoundSourceType) {
+        match sound_source_type {
+            SoundSourceType::WaveSine => {
+                self.send_cc_message(channel, 14, 24);
+            }
+            SoundSourceType::WaveSaw => {
+                self.send_cc_message(channel, 14, 50);
+            }
+            SoundSourceType::WaveNoiseHPF => {
+                self.send_cc_message(channel, 14, 76);
+            }
+            SoundSourceType::WaveNoiseLPF => {
+                self.send_cc_message(channel, 14, 101);
+            }
+            SoundSourceType::WaveNoiseBPF => {
+                self.send_cc_message(channel, 14, 127);
+            }
+        }
+    }
+    pub fn set_modulation_type(&mut self, channel: u8, modulation_type: ModulationType) {
+        match modulation_type {
+            ModulationType::ModExp => {
+                self.send_cc_message(channel, 14, 109);
+            }
+            ModulationType::ModTri => {
+                self.send_cc_message(channel, 14, 118);
+            }
+            ModulationType::ModRand => {
+                self.send_cc_message(channel, 14, 127);
+            }
+        }
+    }
+    pub fn set_amp_eg(&mut self, channel: u8, amp_eg: AmpEg) {
+        match amp_eg {
+            AmpEg::EnvAd => {
+                self.send_cc_message(channel, 14, 121);
+            }
+            AmpEg::EnvExp => {
+                self.send_cc_message(channel, 14, 124);
+            }
+            AmpEg::EnvMult => {
+                self.send_cc_message(channel, 14, 127);
+            }
+        }
+    }
+    pub fn set_param_level(&mut self, channel: u8, param: ParamSoundType, value: u8) {
+        match param {
+            ParamSoundType::Level => {
+                self.send_cc_message(channel, 17, value);
+            }
+            ParamSoundType::Pitch => {
+                self.send_cc_message(channel, 26, value);
+            }
+            ParamSoundType::EgAttack => {
+                self.send_cc_message(channel, 20, value);
+            }
+            ParamSoundType::EgRelease => {
+                self.send_cc_message(channel, 23, value);
+            }
+            ParamSoundType::ModAmount => {
+                self.send_cc_message(channel, 29, value);
+            }
+            ParamSoundType::ModRate => {
+                self.send_cc_message(channel, 46, value);
+            }
+        }
+    }
+
+    fn send_cc_message(&mut self, channel: u8, cc_number: u8, value: u8) {
+        let message: [u8; 3] = [
+            // 1
+            0xb0 | (channel & 0x0f),
+            // 2
+            cc_number & 0x7f,
+            // 3
+            value & 0x7f,
+        ];
+        let _ = self.conn.send(&message);
+        // println!("Send msg -> {:?}", message);
+    }
+}
+
+enum SoundSourceType {
+    WaveSine,
+    WaveSaw,
+    WaveNoiseHPF,
+    WaveNoiseLPF,
+    WaveNoiseBPF,
+}
+enum ModulationType {
+    ModExp,
+    ModTri,
+    ModRand,
+}
+enum AmpEg {
+    EnvAd,
+    EnvExp,
+    EnvMult,
+}
+pub enum ParamSoundType {
+    Level,
+    Pitch,
+    EgAttack,
+    EgRelease,
+    ModAmount,
+    ModRate,
+}
