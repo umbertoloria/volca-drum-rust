eb2b39fbcf9083c198617574a0d2ca92d6bfe940
Umberto Loria <umberto.loria@gmail.com>
2024-12-25 21:21:56 +0100

    Make your own sound!

--- src/drummer.rs
+++ src/drummer.rs
@@ -1,5 +1,4 @@
 use midir::MidiOutputConnection;
-use std::thread::sleep;
 use std::time::Duration;
 
 // Durations
@@ -21,6 +20,8 @@ pub struct Drummer<'a> {
     pub bpm: f64,
 }
 impl Drummer<'_> {
+    // Play
+    /*
     pub fn hit(&mut self, note: u8, instr: u8, duration: Duration) {
         const NOTE_ON_MSG: u8 = 0x90;
         const NOTE_OFF_MSG: u8 = 0x80;
@@ -32,4 +33,113 @@ impl Drummer<'_> {
         sleep(duration.mul_f64(BPM_DEFAULT).div_f64(self.bpm));
         let _ = self.conn.send(&[NOTE_OFF_MSG, note, VELOCITY]);
     }
+    */
+
+    // Set sound
+    pub fn set_sound_source_type(&mut self, channel: u8, sound_source_type: SoundSourceType) {
+        match sound_source_type {
+            SoundSourceType::WaveSine => {
+                self.send_cc_message(channel, 14, 24);
+            }
+            SoundSourceType::WaveSaw => {
+                self.send_cc_message(channel, 14, 50);
+            }
+            SoundSourceType::WaveNoiseHPF => {
+                self.send_cc_message(channel, 14, 76);
+            }
+            SoundSourceType::WaveNoiseLPF => {
+                self.send_cc_message(channel, 14, 101);
+            }
+            SoundSourceType::WaveNoiseBPF => {
+                self.send_cc_message(channel, 14, 127);
+            }
+        }
+    }
+    pub fn set_modulation_type(&mut self, channel: u8, modulation_type: ModulationType) {
+        match modulation_type {
+            ModulationType::ModExp => {
+                self.send_cc_message(channel, 14, 109);
+            }
+            ModulationType::ModTri => {
+                self.send_cc_message(channel, 14, 118);
+            }
+            ModulationType::ModRand => {
+                self.send_cc_message(channel, 14, 127);
+            }
+        }
+    }
+    pub fn set_amp_eg(&mut self, channel: u8, amp_eg: AmpEg) {
+        match amp_eg {
+            AmpEg::EnvAd => {
+                self.send_cc_message(channel, 14, 121);
+            }
+            AmpEg::EnvExp => {
+                self.send_cc_message(channel, 14, 124);
+            }
+            AmpEg::EnvMult => {
+                self.send_cc_message(channel, 14, 127);
+            }
+        }
+    }
+    pub fn set_param_level(&mut self, channel: u8, param: ParamSoundType, value: u8) {
+        match param {
+            ParamSoundType::Level => {
+                self.send_cc_message(channel, 17, value);
+            }
+            ParamSoundType::Pitch => {
+                self.send_cc_message(channel, 26, value);
+            }
+            ParamSoundType::EgAttack => {
+                self.send_cc_message(channel, 20, value);
+            }
+            ParamSoundType::EgRelease => {
+                self.send_cc_message(channel, 23, value);
+            }
+            ParamSoundType::ModAmount => {
+                self.send_cc_message(channel, 29, value);
+            }
+            ParamSoundType::ModRate => {
+                self.send_cc_message(channel, 46, value);
+            }
+        }
+    }
+
+    fn send_cc_message(&mut self, channel: u8, cc_number: u8, value: u8) {
+        let message: [u8; 3] = [
+            // 1
+            0xb0 | (channel & 0x0f),
+            // 2
+            cc_number & 0x7f,
+            // 3
+            value & 0x7f,
+        ];
+        let _ = self.conn.send(&message);
+        println!("Send msg -> {:?}", message);
+    }
+}
+
+enum SoundSourceType {
+    WaveSine,
+    WaveSaw,
+    WaveNoiseHPF,
+    WaveNoiseLPF,
+    WaveNoiseBPF,
+}
+enum ModulationType {
+    ModExp,
+    ModTri,
+    ModRand,
+}
+enum AmpEg {
+    EnvAd,
+    EnvExp,
+    EnvMult,
+}
+pub enum ParamSoundType {
+    Level,
+    Pitch,
+    EgAttack,
+    EgRelease,
+    ModAmount,
+    ModRate,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use crate::drummer::{Drummer, DRUM_HH, DRUM_KICK, DRUM_SNARE, DUR_1_8};
+use crate::drummer::{Drummer, ParamSoundType};
 use crate::midi_controller::init_midi_controller;
+use std::f64::consts::PI;
 use std::io::Write;
+use std::thread::sleep;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
 mod drummer;
 mod midi_controller;
@@ -18,6 +21,7 @@ fn main() {
     };
 
     // SONG PLAY
+    /*
     for _ in 0..(2 * 4) {
         drummer.hit(7, DRUM_HH, DUR_1_8);
     }
@@ -29,7 +33,39 @@ fn main() {
     drummer.hit(7, DRUM_KICK, DUR_1_8);
     drummer.hit(7, DRUM_SNARE, DUR_1_8);
     drummer.hit(7, DRUM_HH, DUR_1_8);
+    */
+
+    drummer.set_param_level(0, ParamSoundType::Level, 127);
+
+    let time_chunk = 1000 * 5;
+
+    loop {
+        let millis_this_very_second = get_now() % time_chunk;
+        let from_0_to_1 = millis_this_very_second as f64 / time_chunk as f64;
+
+        let sine_val = ((from_0_to_1 * 2.0 * PI).sin() + 1.0) / 2.0;
+        let sine_val_u8 = (sine_val * 127.0) as u8;
+
+        // Before, it was: "drummer.send_cc_message(0, 26, sine_val_u8);"
+        drummer.set_param_level(0, ParamSoundType::Pitch, sine_val_u8);
+        show_progress(sine_val);
+
+        sleep(Duration::from_millis(149));
+    }
 
     // MIDI CONNECTION: SHUT-DOWN
     conn.close();
+}
+
+fn get_now() -> u128 {
+    let start = SystemTime::now();
+    let since_the_epoch = start
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+    since_the_epoch.as_millis()
+}
+
+pub fn show_progress(val: f64) {
+    let times = (val * 20.0) as usize;
+    println!("{}", "#".repeat(times));
 }
