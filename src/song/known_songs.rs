f8996a5e34fce9a198232b46f4e8025d45f23053
Umberto Loria <umberto.loria@gmail.com>
2025-02-25 16:58:03 +0100

    Song updated

--- src/song/known_songs.rs
+++ src/song/known_songs.rs
@@ -38,6 +38,7 @@ pub fn get_song_coez_la_musica_non_c_e() -> Song {
                 },
             ),
             (
+                // HH on 1/8ths.
                 "C".into(),
                 DrumPattern {
                     key: "C".into(),
@@ -112,7 +113,8 @@ pub fn get_song_coez_la_musica_non_c_e() -> Song {
                 bars: 3 + 1,
                 time_signature: (4, 4),
                 num_1_16s_in_a_quarter: 4,
-                drum_pattern_key: None,
+                // drum_pattern_key: None, //Always have HH or some sort of click.
+                drum_pattern_key: Some("C".into()),
                 keyboard_pattern_key: Some("A".into()),
                 notes: None,
             },
