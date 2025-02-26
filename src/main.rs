68bc07ff4252080a04f1ed5b0c574b7bd34ef0d2
Umberto Loria <umberto.loria@gmail.com>
2025-02-26 15:44:25 +0100

    Music Thread added

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,34 @@
-use crate::players::player::play_song_example;
-use crate::server::main_server::main_server;
+use crate::music_thread::music_thread::{main_music_thread, CommFromMusicThread};
 
 mod devices;
 mod instruments;
 mod midi;
+mod music_thread;
 mod players;
 mod server;
 mod song;
 mod utils;
 
 fn main() {
-    // SERVER
-    // let server_thread = main_server();
-    main_server();
+    // SOCKET SERVER
+    // main_server();
 
-    play_song_example();
-
-    // server_thread.join().unwrap();
+    // MUSIC THREAD
+    let (music_thread, rx_music_thread) = main_music_thread();
+    for msg in rx_music_thread {
+        match msg {
+            // TODO: Communicate via broadcast with all Clients
+            CommFromMusicThread::SongStarted => {
+                println!("Song started");
+            }
+            CommFromMusicThread::SongPlayingUpdate => {
+                println!("Song playing update");
+            }
+            CommFromMusicThread::SongEnded => {
+                println!("Song ended");
+            }
+        }
+    }
+    music_thread.join().unwrap();
+    println!("Music thread ended!");
 }
---
+++ src/music_thread/music_thread.rs
@@ -0,0 +1,26 @@
+use crate::players::player::play_song_example;
+use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, Sender};
+use std::thread;
+use std::thread::JoinHandle;
+
+pub enum CommFromMusicThread {
+    SongStarted,
+    SongPlayingUpdate,
+    SongEnded,
+}
+type CommFromMusicThreadTx = Sender<CommFromMusicThread>;
+type CommFromMusicThreadRx = Receiver<CommFromMusicThread>;
+pub fn main_music_thread() -> (JoinHandle<()>, CommFromMusicThreadRx) {
+    let (tx, rx) = mpsc::channel::<CommFromMusicThread>();
+    let thread = thread::spawn(|| {
+        music_thread(tx);
+    });
+    (thread, rx)
+}
+
+fn music_thread(tx: CommFromMusicThreadTx) {
+    tx.send(CommFromMusicThread::SongStarted).unwrap();
+    play_song_example();
+    tx.send(CommFromMusicThread::SongEnded).unwrap();
+}
