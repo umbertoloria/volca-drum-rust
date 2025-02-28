09d6ed340be2b986d922c6b3d23b210c03e95642
Umberto Loria <umberto.loria@gmail.com>
2025-02-28 19:03:26 +0100

    Play Queue refactored

--- src/players/play_queue.rs
+++ src/players/play_queue.rs
@@ -6,16 +6,31 @@ use std::thread::JoinHandle;
 // PLAY QUEUE
 type PlayQueueThread = JoinHandle<()>;
 static PLAY_QUEUE: Mutex<Vec<PlayQueueThread>> = Mutex::new(Vec::new());
-fn add_playing_song_to_list(thread: PlayQueueThread) {
-    PLAY_QUEUE.lock().unwrap().push(thread);
-}
+const DEFAULT_SONG_ID: usize = 7;
 
 // EXPOSED
 pub fn play_song_in_queue() {
-    let thread: PlayQueueThread = thread::spawn(move || {
-        play_song_example();
-    });
-    add_playing_song_to_list(thread);
+    let mut play_queue = PLAY_QUEUE.lock().unwrap();
+    if play_queue.is_empty() {
+        let play_queue_thread: PlayQueueThread = thread::spawn(move || {
+            play_song_example();
+        });
+        // Inserting as first element.
+        play_queue.push(play_queue_thread);
+    } else {
+        // Checking the last element.
+        let last_thread = play_queue.last().unwrap();
+        if last_thread.is_finished() {
+            let play_queue_thread: PlayQueueThread = thread::spawn(move || {
+                play_song_example();
+            });
+            // Inserting as last element.
+            play_queue.push(play_queue_thread);
+        } else {
+            // TODO: Add in queue...
+            println!("Unable to play another song!");
+        }
+    }
 }
 
 pub struct PlayQueueState {
@@ -33,10 +48,18 @@ pub fn get_play_queue_state() -> PlayQueueState {
     let play_queue = PLAY_QUEUE.lock().unwrap();
     let mut result = PlayQueueState { items: Vec::new() };
     for i in 0..play_queue.len() {
-        result.items.push(PlayQueueStateItem {
-            id: i,
-            state: PlayQueueStateItemState::Running, // TODO: Update state
-        });
+        let play_queue_item = &play_queue[i];
+        result.items.push(
+            //
+            PlayQueueStateItem {
+                id: i,
+                state: if play_queue_item.is_finished() {
+                    PlayQueueStateItemState::Running
+                } else {
+                    PlayQueueStateItemState::Stopped
+                },
+            },
+        );
     }
     result
 }
