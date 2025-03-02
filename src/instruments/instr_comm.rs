8a96f5b20d04cb9e164b2ad87c553db59211cfd3
Umberto Loria <umberto.loria@gmail.com>
2025-03-02 18:36:08 +0100

    Instrument Comm improved

--- src/instruments/instr_comm.rs
+++ src/instruments/instr_comm.rs
@@ -1,50 +1,36 @@
 use crate::instruments::instrument::Instrument;
-use std::sync::mpsc;
-use std::sync::mpsc::{Receiver, Sender};
+use crate::thread_comm::thread_comm::{
+    create_thread_comm_instances, ThreadCommReceiver, ThreadCommSender,
+};
 
 #[derive(Debug)]
-enum InstrumentCommCommand {
+pub enum InstrumentCommCommand {
     PlaySong(String, u128),
-    // PlayHit(TempoSnapshot), // Deprecated.
     Shutdown,
 }
 pub fn create_instrument_comm() -> (InstrumentCommSender, InstrumentCommReceiver) {
-    let (tx, rx) = mpsc::channel::<InstrumentCommCommand>();
-    let instrument_comm_sender = InstrumentCommSender::new(tx);
-    let instrument_comm_receiver = InstrumentCommReceiver::new(rx);
-    (instrument_comm_sender, instrument_comm_receiver)
+    let (sender, receiver) = create_thread_comm_instances::<InstrumentCommCommand>();
+    let instrument_comm_sender = InstrumentCommSender::new(sender);
+    (instrument_comm_sender, receiver)
 }
 
-// INSTRUMENT COMM SENDER
 pub struct InstrumentCommSender {
-    tx: Sender<InstrumentCommCommand>,
+    sender: ThreadCommSender<InstrumentCommCommand>,
 }
 impl InstrumentCommSender {
-    pub fn new(tx: Sender<InstrumentCommCommand>) -> Self {
-        Self { tx }
+    pub fn new(sender: ThreadCommSender<InstrumentCommCommand>) -> Self {
+        Self { sender }
     }
     pub fn send_command_play_song(&self, song_id: String, start_from_millis: u128) {
         let command = InstrumentCommCommand::PlaySong(song_id, start_from_millis);
-        self.tx.send(command).unwrap();
+        self.sender.send(command);
     }
     pub fn send_command_shutdown(&self) {
         let command = InstrumentCommCommand::Shutdown;
-        self.tx.send(command).unwrap();
+        self.sender.send(command);
     }
 }
-
-// INSTRUMENT COMM RECEIVER
-pub struct InstrumentCommReceiver {
-    rx: Receiver<InstrumentCommCommand>,
-}
-impl InstrumentCommReceiver {
-    pub fn new(rx: Receiver<InstrumentCommCommand>) -> Self {
-        Self { rx }
-    }
-    pub fn loop_requests(self) -> Receiver<InstrumentCommCommand> {
-        self.rx
-    }
-}
+pub type InstrumentCommReceiver = ThreadCommReceiver<InstrumentCommCommand>;
 
 // INSTRUMENT BROADCAST COMM
 pub struct InstrumentBroadcastComm {
@@ -56,13 +42,6 @@ impl InstrumentBroadcastComm {
             instrument_comm_sender.send_command_play_song(song_id.clone(), start_from_millis);
         }
     }
-    /*pub fn play_1_16th(&mut self, tempo_snapshot: &TempoSnapshot) {
-        for tx in &self.tx_list {
-            let cloned_tempo_snapshot = tempo_snapshot.clone();
-            let instr_comm_command = InstrCommCommand::PlayHit(cloned_tempo_snapshot);
-            tx.send(instr_comm_command).unwrap();
-        }
-    }*/
     pub fn shutdown(&self) {
         for instrument_comm_sender in &self.instrument_comm_senders_list {
             instrument_comm_sender.send_command_shutdown();
@@ -73,7 +52,7 @@ pub fn start_listening_to_instrument_comm_commands(
     instrument_comm_receiver: InstrumentCommReceiver,
     instrument: &mut impl Instrument,
 ) {
-    for instrument_comm_command in instrument_comm_receiver.loop_requests() {
+    for instrument_comm_command in instrument_comm_receiver.get_recv_iter() {
         match instrument_comm_command {
             InstrumentCommCommand::PlaySong(song_id, start_from_millis) => {
                 instrument.play_song(song_id, start_from_millis);
--- src/song/mod.rs
+++ src/song/mod.rs
@@ -2,4 +2,4 @@ pub mod composer;
 pub mod known_songs;
 pub mod song;
 pub mod yaml_patch_reader;
-mod yaml_song_reader;
+pub mod yaml_song_reader;
