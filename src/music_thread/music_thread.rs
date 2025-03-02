ff0834d2de69d2382db8ede795e4dec1107a3bfe
Umberto Loria <umberto.loria@gmail.com>
2025-03-02 16:40:02 +0100

    Play Queue Thread improved

--- src/music_thread/music_thread.rs
+++ src/music_thread/music_thread.rs
@@ -2,6 +2,7 @@ use crate::music_thread::music_thread_comm::{MusicThreadCommReceiver, MusicThrea
 use crate::players::play_queue::play_song_example_with_updates;
 use crate::server::main_server_comm::MainThreadCommSender;
 use std::sync::atomic::AtomicBool;
+use std::sync::mpsc::Receiver;
 use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 use std::thread::JoinHandle;
@@ -17,20 +18,39 @@ pub fn main_music_thread(
 
 enum PlayQueueRequest {
     RequestToPlay,
+    CloseThread,
 }
 fn music_thread_logics(
     music_thread_comm_receiver: MusicThreadCommReceiver,
     main_thread_comm_sender: MainThreadCommSender,
 ) {
     let (tx, rx) = mpsc::channel::<PlayQueueRequest>();
+    let play_queue_thread = play_queue_thread(rx, main_thread_comm_sender);
 
+    for music_thread_request in music_thread_comm_receiver.loop_requests() {
+        match music_thread_request {
+            MusicThreadRequest::PlaySong() => {
+                tx.send(PlayQueueRequest::RequestToPlay).unwrap();
+            }
+        }
+    }
+
+    tx.send(PlayQueueRequest::CloseThread).unwrap();
+    play_queue_thread.join().unwrap();
+}
+
+fn play_queue_thread(
+    rx: Receiver<PlayQueueRequest>,
+    main_thread_comm_sender: MainThreadCommSender,
+) -> JoinHandle<()> {
     let is_playing = Arc::new(Mutex::new(AtomicBool::new(false)));
-
-    let play_queue_thread = thread::spawn(move || {
+    thread::spawn(move || {
         for request in rx {
             match request {
                 PlayQueueRequest::RequestToPlay => {
                     let main_thread_comm_sender_clone = main_thread_comm_sender.clone();
+
+                    // Atomic Read
                     let mut atomic_bool = is_playing.lock().unwrap();
                     let mut value = atomic_bool.get_mut();
                     if *value {
@@ -44,17 +64,10 @@ fn music_thread_logics(
                         *value = false;
                     }
                 }
+                PlayQueueRequest::CloseThread => {
+                    break;
+                }
             }
         }
-    });
-
-    for music_thread_request in music_thread_comm_receiver.loop_requests() {
-        match music_thread_request {
-            MusicThreadRequest::PlaySong() => {
-                tx.send(PlayQueueRequest::RequestToPlay).unwrap();
-            }
-        }
-    }
-
-    // play_queue_thread.abort();
+    })
 }
