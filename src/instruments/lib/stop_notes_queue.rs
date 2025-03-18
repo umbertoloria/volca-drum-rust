b92036cf054aa3895481e307a6b0d1f514a1ff52
Umberto Loria <umberto.loria@gmail.com>
2025-03-18 22:38:38 +0100

    StopNotesQueue refactored

--- src/instruments/lib/stop_notes_queue.rs
+++ src/instruments/lib/stop_notes_queue.rs
@@ -1,37 +1,40 @@
 use crate::music::note::Note;
 use std::collections::{HashMap, HashSet};
 
+type SetNotesToStop = HashSet<String>;
 pub struct StopNotesQueue {
-    stop_notes_queue: HashMap<usize, HashSet<String>>,
+    // From Index 1/16th to Set of Notes.
+    queue_i2set: HashMap<usize, SetNotesToStop>,
 }
 impl StopNotesQueue {
     pub fn new() -> Self {
         Self {
-            stop_notes_queue: HashMap::new(),
+            queue_i2set: HashMap::new(),
         }
     }
     pub fn add_notes_to_stop(&mut self, notes: &Vec<Note>, index_1_16th: usize) {
         // Param "index_1_16th" starts from 1.
-        if let Some(queued_notes_to_stop) = self.stop_notes_queue.get_mut(&index_1_16th) {
+        if let Some(set) = self.queue_i2set.get_mut(&index_1_16th) {
             for note in notes {
-                queued_notes_to_stop.insert(note.to_hash());
+                let item = note.to_hash();
+                set.insert(item);
             }
         } else {
-            let mut queued_notes_to_stop = HashSet::new();
+            let mut set = SetNotesToStop::new();
             for note in notes {
-                queued_notes_to_stop.insert(note.to_hash());
+                let item = note.to_hash();
+                set.insert(item);
             }
-            self.stop_notes_queue
-                .insert(index_1_16th, queued_notes_to_stop);
+            self.queue_i2set.insert(index_1_16th, set);
         }
     }
     pub fn dequeue_notes_at_this_1_16th(&mut self, index_1_16th: usize) -> Option<Vec<Note>> {
         // Param "index_1_16th" starts from 1.
-        if let Some(queued_notes_to_stop) = self.stop_notes_queue.remove(&index_1_16th) {
+        if let Some(set) = self.queue_i2set.remove(&index_1_16th) {
             let mut notes_to_stop = Vec::new();
-            for note_hash in queued_notes_to_stop {
-                let note = Note::from_hash(note_hash);
-                notes_to_stop.push(note);
+            for item in set {
+                let note_to_stop = Note::from_hash(item);
+                notes_to_stop.push(note_to_stop);
             }
             Some(notes_to_stop)
         } else {
@@ -51,7 +54,7 @@ impl StopNoteQueueForBass {
         }
     }
     pub fn add_note_to_stop(&mut self, note: &Note, index_1_16th: usize) {
-        // TODO: Avoid cloning Note string
+        // TODO: Avoid cloning Note
         let notes = vec![note.clone()];
         self.inner.add_notes_to_stop(&notes, index_1_16th);
     }
