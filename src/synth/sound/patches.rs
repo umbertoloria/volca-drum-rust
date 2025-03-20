6f87c961034aa994a15c97b40f7465531559d300
Umberto Loria <umberto.loria@gmail.com>
2025-03-20 13:22:39 +0100

    Patches improved

--- src/synth/sound/patches.rs
+++ src/synth/sound/patches.rs
@@ -2,7 +2,9 @@ use crate::instruments::lib::drum_sounds::DrumSound;
 use crate::synth::lfo::lfo::LFO;
 use crate::synth::mono_synth::SynthSoundType;
 use crate::synth::oscillator::wave_table_oscillator::WaveTableOscillator;
-use crate::synth::oscillator::wave_tables::{create_wt_noise, create_wt_saw, create_wt_square};
+use crate::synth::oscillator::wave_tables::{
+    create_wt_noise, create_wt_saw, create_wt_sine, create_wt_square,
+};
 use crate::synth::sound::synth_patch_injector::SynthPatchInjector;
 use crate::utils::timing::get_now_millis;
 
@@ -22,8 +24,8 @@ impl SynthPatch {
     }
     pub fn get_sample(&self, t0_ms: u128, index: f32, synth_sound_type: SynthSoundType) -> f32 {
         match self {
-            SynthPatch::Bass1 => patch_keys_1(t0_ms, index),
-            SynthPatch::Keys1 => patch_bass_1(t0_ms, index),
+            SynthPatch::Keys1 => patch_keys_1(t0_ms, index),
+            SynthPatch::Bass1 => patch_bass_1(t0_ms, index),
             SynthPatch::MetronomeClick => patch_metronome_click(t0_ms, index),
             SynthPatch::Drums1 => patch_drums_1(t0_ms, index, synth_sound_type),
         }
@@ -32,16 +34,16 @@ impl SynthPatch {
 
 // PATCH KEYS 1
 pub fn make_patch_keys_1() -> SynthPatchInjector {
-    SynthPatchInjector::new(SynthPatch::Keys1, 0.3)
+    SynthPatchInjector::new(SynthPatch::Keys1, 1.0)
 }
 fn patch_keys_1(t0_ms: u128, index: f32) -> f32 {
     // 1. Oscillator
-    let oscillator = WaveTableOscillator::new(create_wt_square());
+    let oscillator = WaveTableOscillator::new(create_wt_sine());
     let oscillator_value = oscillator.lerp(index);
 
     // 2. LFO
     let ms = get_now_millis() - t0_ms;
-    let lfo = LFO::new(17, 300, 0.3);
+    let lfo = LFO::new(80, 300, 0.8);
     let lfo_value = lfo.get_value(ms);
 
     oscillator_value * lfo_value
@@ -49,12 +51,11 @@ fn patch_keys_1(t0_ms: u128, index: f32) -> f32 {
 
 // PATCH BASS 1
 pub fn make_patch_bass_1() -> SynthPatchInjector {
-    SynthPatchInjector::new(SynthPatch::Bass1, 0.4)
+    SynthPatchInjector::new(SynthPatch::Bass1, 0.1)
 }
 fn patch_bass_1(t0_ms: u128, index: f32) -> f32 {
     // 1. Oscillator
-    // let oscillator = WaveTableOscillator::new(create_wt_sine());
-    let oscillator = WaveTableOscillator::new(create_wt_saw());
+    let oscillator = WaveTableOscillator::new(create_wt_square());
     let oscillator_value = oscillator.lerp(index);
 
     // 2. LFO
@@ -67,7 +68,7 @@ fn patch_bass_1(t0_ms: u128, index: f32) -> f32 {
 
 // PATCH METRONOME CLICK
 pub fn make_patch_metronome_click() -> SynthPatchInjector {
-    SynthPatchInjector::new(SynthPatch::MetronomeClick, 0.5)
+    SynthPatchInjector::new(SynthPatch::MetronomeClick, 0.2)
 }
 fn patch_metronome_click(t0_ms: u128, index: f32) -> f32 {
     // 1. Oscillator
